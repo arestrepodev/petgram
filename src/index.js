2155b57727a570325c4a37169a3f584be5bdbcf2
Arnold Restrepo <arnold.restrepo@quick.com.co>
2019-08-19 21:45:07 -0500

    Editor Config and Install React, Babel Dependencies

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-console.log("Ready!");
-document.write("Hello");
+import React from "react";
+import { render } from "react-dom";
+
+const $app_element = document.getElementById("app");
+
+render(<h1>Hello</h1>, $app_element);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
 module.exports = {
   output: {
-    filename: "app.bundle.js"
+    filename: 'app.bundle.js'
   },
   devServer: {
-    port: 8000
+    port: 9000
   },
-  plugins: [new HtmlWebpackPlugin()]
-};
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+}
