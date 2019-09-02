fb2207ceee2905f0be3e32be9ecaa2676208e23b
Arnold Restrepo <arnold.restrepo@quick.com.co>
2019-09-01 19:00:30 -0500

    Logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { ListOfCategories } from "./components/ListOfCategories";
 import { GlobalStyles } from "./GlobalStyles";
 import { ListOfPhotoCards } from "./components/ListOfPhotoCards";
+import {Logo} from "./components/Logo" 
 
 export const App = () => {
 	return (
-		<div>
+		<>
+			<Logo/>
 			<GlobalStyles />
 			<ListOfCategories />
 			<ListOfPhotoCards />
-		</div>
+		</>
 	);
 };
---
+++ src/components/Logo/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Svg = styled.svg`
+	width: 250px;
+	height: 130px;
+	margin-top: -40px;
+	margin-left: -18px;
+`;
