afd4aa45e4ec9c6f792c1c4bf574a04151bad7b9
Arnold Restrepo <arnold.restrepo@quick.com.co>
2019-09-09 14:05:11 -0500

    useEffect Clear

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -4,6 +4,7 @@ import { List, Item } from "./styles";
 
 export const ListOfCategories = () => {
 	const [categories, setCategories] = useState([]);
+	const [showFixed, setShowFixed] = useState(false);
 
 	useEffect(() => {
 		const getCategories = async () => {
@@ -16,13 +17,30 @@ export const ListOfCategories = () => {
 		getCategories();
 	}, []);
 
-	return (
-		<List>
+	useEffect(() => {
+		const onScroll = event => {
+			const newShowFixed = window.scrollY > 200;
+			showFixed !== newShowFixed && setShowFixed(newShowFixed);
+		};
+		document.addEventListener("scroll", onScroll);
+		return () => {
+			document.removeEventListener("scroll", onScroll);
+		};
+	}, [showFixed]);
+
+	const renderList = fixed => (
+		<List className={fixed ? "fixed" : ""}>
 			{categories.map(category => (
 				<Item key={category.id}>
 					<Category {...category} />
 				</Item>
 			))}
 		</List>
+	);
+
+	return (
+		<>
+			{renderList()} {showFixed && renderList(true)}
+		</>
 	);
 };
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -6,6 +6,18 @@ export const List = styled.ul`
 	overflow-x: auto;
 	width: 100%;
 	list-style: none;
+	&.fixed {
+		background: #fff;
+		border-radius: 60px;
+		box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+		/* left: 0; */
+		max-width: 500px;
+		z-index: 99;
+		padding: 8px;
+		position: fixed;
+		top: -20px;
+		transform: scale(0.5);
+	}
 `;
 
 export const Item = styled.li`
