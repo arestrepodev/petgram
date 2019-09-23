fb45f1eaa51442a1d8d3dfc521d0bf7cc2bcd068
Arnold Restrepo <arnold.restrepo@quick.com.co>
2019-09-22 22:16:25 -0500

    GraphQL - Apollo Client

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { App } from "./App";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+const client = new ApolloClient({
+	// uri: "https://petgram-server.midudev.now.sh/graphql"
+	uri: "https://petgram-server-arnold.restrepoarnold.now.sh/graphql"
+});
 
 const $app = document.getElementById("app");
 
-ReactDom.render(<App />, $app);
+ReactDom.render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	$app
+);
