e567164743613bcd09a96756a09805cd9a4e5ebe
Arnold Restrepo <restrepo_arnold@outlook.com>
2019-10-27 19:50:29 -0500

    Context export and Persisted Session

--- src/views/NotRegisterUser.js
+++ src/views/NotRegisterUser.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
-import Context from '../Context';
+import { Context } from '../Context';
 import { UserForm } from '../components/UserForm';
 import { useRegisterMutation } from '../hooks/useRegisterMutation';
 import { useLoginMutation } from '../hooks/useLoginMutation';
 
 export const NotRegisteredUser = () => {
+	const { activateAuth } = useContext(Context);
 	// Register Mutation Hook
 	const {
 		register,
@@ -22,46 +23,46 @@ export const NotRegisteredUser = () => {
 		loading: loadingLogin
 	} = useLoginMutation();
 
+	const handleRegister = ({ email, password }) => {
+		register(email, password)
+			.then(({ data }) => {
+				const { signup } = data;
+				console.log(signup);
+				activateAuth();
+			})
+			.catch(error => console.error(error));
+	};
+
+	const handleLogin = ({ email, password }) => {
+		login(email, password)
+			.then(({ data }) => {
+				const { login } = data;
+				activateAuth(login);
+			})
+			.catch(error => console.error(error));
+	};
+
+	const errorMsgRegister =
+		errorRegister && 'El usuario ya existe o hay algún problema.';
+
+	const errorMsgLogin = errorLogin && 'El email o la contraseña son inválidos.';
+
 	return (
-		<Context.Consumer>
-			{({ isAuth, activateAuth }) => {
-				const handleRegister = ({ email, password }) => {
-					register(email, password)
-						.then(activateAuth)
-						.catch(error => console.error(error));
-				};
-
-				const handleLogin = ({ email, password }) => {
-					login(email, password)
-						.then(activateAuth)
-						.catch(error => console.error(error));
-				};
-
-				const errorMsgRegister =
-					errorRegister && 'El usuario ya existe o hay algún problema.';
-
-				const errorMsgLogin =
-					errorLogin && 'El email o la contraseña son inválidos.';
-
-				return (
-					<>
-						<UserForm
-							onSubmit={handleRegister}
-							title='Registarse'
-							disabled={loadingRegister}
-							error={errorMsgRegister}
-							loading={loadingRegister}
-						/>
-						<UserForm
-							onSubmit={handleLogin}
-							title='Iniciar Sesión'
-							disabled={loadingLogin}
-							error={errorMsgLogin}
-							loading={loadingLogin}
-						/>
-					</>
-				);
-			}}
-		</Context.Consumer>
+		<>
+			<UserForm
+				onSubmit={handleRegister}
+				title='Registarse'
+				disabled={loadingRegister}
+				error={errorMsgRegister}
+				loading={loadingRegister}
+			/>
+			<UserForm
+				onSubmit={handleLogin}
+				title='Iniciar Sesión'
+				disabled={loadingLogin}
+				error={errorMsgLogin}
+				loading={loadingLogin}
+			/>
+		</>
 	);
 };
