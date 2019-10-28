19c26e5c2cb7e8628dd8539dbbe54c8b224b3c96
Arnold Restrepo <restrepo_arnold@outlook.com>
2019-10-27 21:50:49 -0500

    Error Api Graphql

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,15 +1,14 @@
-import React, { useEffect, useRef, useState } from "react";
-import { ImgWrapper, Img, Article } from "./styles";
-import { useLocalStorage } from "../../hooks/useLocalStorage";
-import { useNearScreen } from "../../hooks/useNearScreen";
-import { FavButton } from "../FavButton";
-import { useMutation } from "@apollo/react-hooks";
-import { Link } from "@reach/router";
-import gql from "graphql-tag";
+import React from 'react';
+import { ImgWrapper, Img, Article } from './styles';
+import { useNearScreen } from '../../hooks/useNearScreen';
+import { FavButton } from '../FavButton';
+import { useMutation } from '@apollo/react-hooks';
+import { Link } from '@reach/router';
+import gql from 'graphql-tag';
 
 const LIKE_PHOTO = gql`
-	mutation likeAnonymousPhoto($input: LikePhoto!) {
-		likeAnonymousPhoto(input: $input) {
+	mutation likePhoto($input: LikePhoto!) {
+		likePhoto(input: $input) {
 			id
 			liked
 			likes
@@ -18,19 +17,16 @@ const LIKE_PHOTO = gql`
 `;
 
 const DEFAULT_IMAGE =
-	"https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png";
+	'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png';
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-	const key = `like-${id}`;
-	const [liked, setLiked] = useLocalStorage(key, false);
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
 	const [show, element] = useNearScreen();
 	const [toggleLike] = useMutation(LIKE_PHOTO, {
 		variables: { input: { id } }
 	});
 
 	const handleFavClick = () => {
-		!liked && toggleLike();
-		setLiked(!liked);
+		toggleLike();
 	};
 
 	return (
@@ -39,7 +35,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
 				<>
 					<Link to={`/detail/${id}`}>
 						<ImgWrapper>
-							<Img src={src} alt="User" />
+							<Img src={src} alt='User' />
 						</ImgWrapper>
 					</Link>
 					<FavButton liked={liked} likes={likes} onClick={handleFavClick} />
--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from "react";
-import ReactDom from "react-dom";
-import { App } from "./App";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
-import Context from "./Context";
+import React from 'react';
+import ReactDom from 'react-dom';
+import { App } from './App';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import Context from './Context';
 const client = new ApolloClient({
 	// uri: "https://petgram-server.midudev.now.sh/graphql"
-	uri: "https://petgram-server-arnold.restrepoarnold.now.sh/graphql"
+	uri: 'https://petgram-server-arnold.restrepoarnold.now.sh/graphql',
+	request: operation => {
+		const token = window.sessionStorage.getItem('token');
+		const authorization = token ? `bearer ${token}` : '';
+		operation.setContext({
+			headers: {
+				authorization
+			}
+		});
+	},
+	onError: error => {
+		const { networkError } = error;
+		if (networkError && networkError.result.code === 'invalid token') {
+			window.sessionStorage.removeItem('token');
+			window.location.href = '/';
+		}
+	}
 });
 
-const $app = document.getElementById("app");
+const $app = document.getElementById('app');
 
 ReactDom.render(
 	<Context.Provider>
