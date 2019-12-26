10f3dcd80e97f22b1ce12968b613800087c98f08
Arnold Restrepo <restrepo_arnold@outlook.com>
2019-12-25 22:55:39 -0500

    React Helmet

--- src/views/Favs.js
+++ src/views/Favs.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import ListOfFavs from "../components/ListOfFavs";
 
 export const Favs = () => (
 	<section>
+		<Helmet>
+			<title>Petgram - Tus Favoritos</title>
+			<meta
+				name="description"
+				content="Tus fotos favoritas en un solo lugar"
+			></meta>
+		</Helmet>
 		<h1>Favorites</h1>
 		<ListOfFavs />
 	</section>
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import { ListOfCategories } from "../components/ListOfCategories";
 import { ListOfPhotoCards } from "../components/ListOfPhotoCards";
 
 export const Home = ({ categoryId }) => {
 	return (
 		<>
+			<Helmet>
+				<title>Petgram - Tu App de Fotos de Mascotas</title>
+				<meta
+					name="description"
+					content="En Petgram puedes encontrar fotos de animales domesticos muy bonitos"
+				></meta>
+			</Helmet>
 			<ListOfCategories />
 			<ListOfPhotoCards categoryId={categoryId} />
 		</>
