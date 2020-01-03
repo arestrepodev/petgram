7001faa0b829d3b7e9bbdb5133722f512a145207
Arnold Restrepo <restrepo_arnold@outlook.com>
2020-01-03 11:15:33 -0500

    PWA Service Worker

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 const webpack = require("webpack");
+const WebpackPwaManifestPlugin = require("webpack-pwa-manifest");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
 	entry: {
@@ -20,6 +22,41 @@ module.exports = {
 		new webpack.HotModuleReplacementPlugin(),
 		new HtmlWebpackPlugin({
 			template: "src/index.html"
+		}),
+		new WebpackPwaManifestPlugin({
+			name: "Petgram - Tu App de fotos de mascotas.",
+			short_name: "Petgram 🐶",
+			description: "Con Petgram puedes encontrar fotos de animales domésticos.",
+			background_color: "#fff",
+			theme_color: "#b1a",
+			icons: [
+				{
+					src: path.resolve("src/assets/icon.png"),
+					sizes: [96, 128, 192, 256, 384, 512]
+				}
+			]
+		}),
+		new WorkboxWebpackPlugin.GenerateSW({
+			runtimeCaching: [
+				{
+					urlPattern: new RegExp(
+						"https://(res.cloudinary.com|images.unsplash.com)"
+					),
+					handler: "CacheFirst",
+					options: {
+						cacheName: "images"
+					}
+				},
+				{
+					urlPattern: new RegExp(
+						"https://petgram-server-arnold.restrepoarnold.now.sh/graphql"
+					),
+					handler: "NetworkFirst",
+					options: {
+						cacheName: "api"
+					}
+				}
+			]
 		})
 	],
 	module: {
